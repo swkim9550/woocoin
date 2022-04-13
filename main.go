c367f5cb0ab7ae1d3f41bcb6629fa48b76b8ad55
soengwookim <kbkkim2@naver.com>
2022-04-13 14:27:03 +0900

    feat : blockchain rest api add

--- main.go
+++ main.go
@@ -1,41 +1,7 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-)
-
-const port string = ":4000"
-
-type URLDescription struct {
-	URL         string `json:"url"`
-	Method      string `json:"method"`
-	Description string `json:"description"`
-	Payload     string `json:"payload,omitempty"`
-}
-
-func documentation(rw http.ResponseWriter, r *http.Request) {
-	data := []URLDescription{
-		{
-			URL:         "/",
-			Method:      "GET",
-			Description: "See Documentation",
-		},
-		{
-			URL:         "/",
-			Method:      "GET",
-			Description: "See Documentation",
-			Payload:     "data:string",
-		},
-	}
-	rw.Header().Add("Content-Type", "application/json")
-	json.NewEncoder(rw).Encode(data)
-}
+import "woocoin/rest"
 
 func main() {
-	http.HandleFunc("/", documentation)
-	fmt.Printf("Listening on http://localhost:%s\n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	rest.Start(4000)
 }
