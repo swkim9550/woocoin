0e63947ea6ada912b38618d5be8896b1ef4f672b
soengwookim <kbkkim2@naver.com>
2022-04-16 16:20:47 +0900

    feat : util toByte,FromByte add

--- utils/utils.go
+++ utils/utils.go
@@ -1,9 +1,26 @@
 package utils
 
-import "log"
+import (
+	"bytes"
+	"encoding/gob"
+	"log"
+)
 
 func HandleErr(err error) {
 	if err != nil {
 		log.Panicln(err)
 	}
+}
+
+/* change Tobyte everything*/
+func ToByte(i interface{}) []byte {
+	var aBuffer bytes.Buffer
+	encoder := gob.NewEncoder(&aBuffer)
+	HandleErr(encoder.Encode(i))
+	return aBuffer.Bytes()
+}
+
+func FromBytes(i interface{}, data []byte) {
+	encoder := gob.NewDecoder(bytes.NewReader(data))
+	HandleErr(encoder.Decode(i))
 }
